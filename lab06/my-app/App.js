d76028b7d181a73165d18bea89190101363fe47b
David <31216720+Agnarr43@users.noreply.github.com>
2020-10-09 21:48:54 -0400

    lab 6 fixed

---
+++ lab06/my-app/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
+
+export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=search-terms&key=AIzaSyDmQi-A0eR12p9FhsSJtmT20W4YffjW0mM')
+    //fetch('https://reactnative.dev/movies.json')  
+      .then((response) => response.json())
+      .then((json) => setData(json.items))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      {isLoading ? <ActivityIndicator/> : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Text>{item.volumeInfo.title}: {item.volumeInfo.publisher}, {item.volumeInfo.publishedDate}</Text>
+          )}
+        />
+      )}
+    </View>
+  );
+};
